1934f1abb785295b077c8eb6a131b050edf6418b
Damien Boisvert <damienboisvert8@gmail.com>
2023-12-21 13:27:28 -0800

    Add a favicon

---
+++ assets/js/externalLink.js
@@ -0,0 +1,10 @@
+console.log("externalLink.js OK")
+(function(){
+    for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
+        var b = c[a];
+        if(b.getAttribute("href") && b.hostname !== location.hostname) {
+            b.target = "_blank";
+            b.rel = "noopener";
+        }
+    }
+})();
