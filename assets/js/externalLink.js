40fb35bbb04a05f1de71bff70e88ce475fe356f4
Damien Boisvert <damien@alphagame.dev>
2024-09-04 16:19:52 +0000

    Open external links and PDFs in a new tab

--- assets/js/externalLink.js
+++ assets/js/externalLink.js
@@ -1,5 +1,6 @@
-console.log("externalLink.js OK")
-(function(){
+//open external links in a new window
+function external_new_window() {
+    console.log("external_new_window: OK")
     for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
         var b = c[a];
         if(b.getAttribute("href") && b.hostname !== location.hostname) {
@@ -7,4 +8,25 @@ console.log("externalLink.js OK")
             b.rel = "noopener";
         }
     }
-})();
+}
+//open PDF links in a new window
+function pdf_new_window ()
+{
+    console.log("pdf_new_window: OK")
+    if (!document.getElementsByTagName) return false;
+    var links = document.getElementsByTagName("a");
+    for (var eleLink=0; eleLink < links.length; eleLink ++) {
+    if ((links[eleLink].href.indexOf('.pdf') !== -1)||(links[eleLink].href.indexOf('.doc') !== -1)||(links[eleLink].href.indexOf('.docx') !== -1)) {
+        links[eleLink].onclick =
+        function() {
+            window.open(this.href);
+            return false;
+        }
+    }
+    }
+} 
+
+$('document').ready(function() {
+    external_new_window();
+    pdf_new_window();
+})
